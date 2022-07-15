3bec37617c9434cf8ace7128f4ea1b2a0846a665
caiolaytynher <caiolaytynherbr@gmail.com>
2022-07-15 04:35:47 -0300

    added a qtile version.

---
+++ qtile_version.py
@@ -0,0 +1,28 @@
+import yaml
+import sys
+
+INPUT_DIR = "./yaml-files"
+OUTPUT_DIR = "./output-files"
+
+
+def main(args: list[str]) -> None:
+    if len(args) > 1:
+        filename: str = args[1]
+    else:
+        raise Exception("No file name provided.")
+
+    with open(f"{INPUT_DIR}/{filename}.yaml", "r") as input_file:
+        color_pallete: dict = yaml.safe_load(input_file)
+
+    with open(f"{OUTPUT_DIR}/{filename}.txt", "w") as output_file:
+        output_file.write(f"ColorScheme(\n")
+        for color_type, color_contents in color_pallete.items():
+            output_file.write(f"\t{color_type}={color_type.capitalize()}(\n")
+            for color_name, color_hex in color_contents.items():
+                output_file.write(f"\t\t{color_name}=[{color_hex!r}, {color_hex!r}],\n")
+            output_file.write(f"\t),\n")
+        output_file.write(")")
+
+
+if __name__ == "__main__":
+    main(sys.argv)
